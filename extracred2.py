5aaa8c292559fda0c4968be635e23159c76d746f
DeanLiberman <38141834+DeanLiberman@users.noreply.github.com>
2023-01-17 11:40:10 -0500

    Add files via upload
    dumb little kinder themed egg room for dumb text adventure

---
+++ extracred2.py
@@ -0,0 +1,26 @@
+print "Is the real reason behind the banning of the kinder surprise egg something to do with Germans?"
+print "Or is the real reason behind the banning of Kinder Surprise a surprise, HENCE THE SURPRISE?!"
+
+
+def kinderroom():
+            print "Determined to figure out the mystery of the kinder surprise egg"
+            print "you enter into an egg shaped room called the kinderroom."
+            print "You don't see any macoroni art, so you're pretty sure you're not"
+            print "back in Kindergarten. Suddenly, a golden Swatiska(if you're a Nazi)"
+            print "or a golden Buddhist Swatiska flashes before your eyes(if not a Nazi)"
+            print "A loud disembodied voice that is either the Buddha"
+            print "or Adolf Hitler respectfully says 'choose and eat your egg.'"
+            print "You see an egg on the left and an egg on the right. You decide to eat the egg on the..."
+            
+            choice = raw_input("> ")
+            
+            if choice == "left":
+                print "You choke on the plastic toy inside and die."
+                exit(1)
+            if choice == "right":
+                print "You discover the mystery of the egg and win!"
+                exit(1)
+kinderroom()
+    
+    
+
